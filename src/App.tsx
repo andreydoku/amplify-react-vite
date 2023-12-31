049a75a5b20633a86115b2220a759c5314ffb33a
Andrey Dokuchayev <andreydoku@gmail.com>
2023-12-31 14:57:30 -0600

    mike

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import mikeImage from './assets/mike-awad.jpeg'
+
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
 
+	const text1 = "I got Awad of cash, y'all!";
+	const text2 = "but... Mo money, Mo problems!";
+	
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		<div className='app'>
+			<p>{text1}</p>
+			<img src={mikeImage} alt="" className='mike-image' />
+			<p>{text2}</p>
+		</div>
 	)
 }
 
